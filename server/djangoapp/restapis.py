c561bfe987a298a012c6002d1d63606e147281b6
Suman-Mukherje <sumakaut2000@gmail.com>
2024-09-30 20:38:53 +0530

    modified

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -4,15 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+backend_url = os.getenv(
+    'backend_url', 
+    default="http://localhost:3030"
+)
+sentiment_analyzer_url = os.getenv(
+    'sentiment_analyzer_url', 
+    default="http://localhost:5050/"
+)
 
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
-        for key, value in kwargs.items():  # E275: Fixed missing whitespace after keyword
-            params = params + key + "=" + value + "&"  # E231, E225: Added whitespaces around operators
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
@@ -21,7 +27,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as e:  # E722: Avoid bare 'except', use 'Exception'
+    except Exception as e:
         # If any error occurs
         print(f"Network exception occurred: {e}")
 
@@ -40,8 +46,8 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url, json=data_dict)  # E231: Added whitespace after comma
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except Exception as e:  # E722: Avoid bare 'except'
+    except Exception as e:
         print(f"Network exception occurred: {e}")
